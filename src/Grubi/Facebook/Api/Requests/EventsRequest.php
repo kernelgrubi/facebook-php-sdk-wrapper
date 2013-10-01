7a8750a4e477dc678ad2092dbd8bb29b791f6ea7
Jessé Alves Galdino (jgskin) <galdino.jesse@gmail.com>
2013-10-01 17:43:17 -0300

    Event check admin permissions functionality

--- src/Grubi/Facebook/Api/Requests/EventsRequest.php
+++ src/Grubi/Facebook/Api/Requests/EventsRequest.php
@@ -35,4 +35,26 @@ class EventsRequest extends BaseRequest {
     public function noResponse() {
         return new FacebookIterator($this->sdk, $this->buildRequestUri("{$this->id}/noreply"));
     }
+
+    /**
+     * check if the authenticated user has administration/host permissions on the event
+     */
+    public function hasAdminPermissions()
+    {
+        $event = $this->one();
+
+        try {
+            // edit the description field to test the user admin permissions
+            return $this->sdk->api($this->id, 'POST',
+                array('description' => $event['description']));
+        } catch (\FacebookApiException $e) {
+            if ($e->getResult()['error']['code'] !== 200) {
+                // any other facebook api error
+                throw $e;
+            }   
+        }
+
+        // no admin permissions facebook api error
+        return false;
+    }
 }
--- tests/Grubi/Facebook/Api/Requests/EventsRequestTest.php
+++ tests/Grubi/Facebook/Api/Requests/EventsRequestTest.php
@@ -4,6 +4,10 @@ namespace Grubi\Facebook\Api\Requests;
 use Grubi\Facebook\Api\Requests\EventsRequest;
 
 class EventsRequestTest extends \PHPUnit_Framework_TestCase {
+
+    private static $resultErrorBoilerplate = array('error' =>
+        array('message' => 'something', 'code' => null));
+
     public function setUp() {
         $this->sdkMock = $this->getMockBuilder('\Facebook')
             ->disableOriginalConstructor()
@@ -75,5 +79,75 @@ class EventsRequestTest extends \PHPUnit_Framework_TestCase {
 
         $iterator = $this->request->id($eventId)->users()->noResponse();
         $iterator->result();
-    }    
+    }
+
+    /**
+     * case: user has admin permissions
+     */
+    public function testHasAdminPermissionsTrue()
+    {
+        $returnedEvent = array('description' => 'a description');
+        $serverResponse = true;
+        $this->sdkMock->expects($this->exactly(2))
+            ->method('api')
+            ->will($this->onConsecutiveCalls($returnedEvent, $serverResponse))
+        ;
+
+        $this->assertTrue($this->request->id('aneventid')->hasAdminPermissions());
+    }
+
+    /**
+     * case: user does not have admin permissions
+     */
+    public function testHasAdminPermissionsFalse()
+    {   
+        $execStatus = false;
+        $apicalls = function () use(&$execStatus) {
+            if ($execStatus) {
+                $resultError = self::$resultErrorBoilerplate;
+                $resultError['error']['code'] = 200;
+                throw new \FacebookApiException($resultError);
+            }
+
+            $execStatus = true;
+
+            return array('description' => 'any description');
+        };
+
+        $this->sdkMock->expects($this->exactly(2))
+            ->method('api')
+            ->will($this->returnCallback($apicalls))
+        ;
+
+        $this->assertFalse($this->request->id('aneventid')->hasAdminPermissions());
+    }
+
+    /**
+     * case: unknown api error
+     */
+    public function testHasAdminPermissionsUnknownError()
+    {   
+        $execStatus = false;
+        $apicalls = function () use(&$execStatus) {
+            if ($execStatus) {
+                $resultError = self::$resultErrorBoilerplate;
+                $resultError['error']['code'] = 290;
+                throw new \FacebookApiException($resultError);
+            }
+
+            $execStatus = true;
+
+            return array('description' => 'any description');
+        };
+
+        $this->sdkMock->expects($this->exactly(2))
+            ->method('api')
+            ->will($this->returnCallback($apicalls))
+        ;
+
+        try {
+            $this->request->id('aneventid')->hasAdminPermissions();
+            $this->fail();
+        } catch (\FacebookApiException $e) {}
+    }
 }
